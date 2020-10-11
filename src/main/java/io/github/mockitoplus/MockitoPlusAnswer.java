9d0cb936810a4aefb0b7af8921d3e3721124e42e
Sean C. Sullivan <github@seansullivan.com>
2020-10-10 19:05:24 -0700

    add type parameter to MockitoPlusAnswer

--- src/main/java/io/github/mockitoplus/MockitoPlusAnswer.java
+++ src/main/java/io/github/mockitoplus/MockitoPlusAnswer.java
@@ -7,21 +7,21 @@ import java.security.SecureRandom;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class MockitoPlusAnswer implements Answer {
-    private final Object value;
+public class MockitoPlusAnswer<T> implements Answer<T> {
+    private final T value;
     private final ExceptionFactory exceptionFactory;
     private final FailureMode failureMode;
     private final AtomicInteger invocationCount = new AtomicInteger(0);
     private final Random random = new SecureRandom();
 
-    public MockitoPlusAnswer(Object value, FailureMode failureMode, ExceptionFactory exFactory) {
+    public MockitoPlusAnswer(T value, FailureMode failureMode, ExceptionFactory exFactory) {
         this.value = value;
         this.exceptionFactory = exFactory;
         this.failureMode = failureMode;
     }
 
     @Override
-    public Object answer(InvocationOnMock invocation) throws Throwable {
+    public T answer(InvocationOnMock invocation) throws Throwable {
         final int n = invocationCount.getAndIncrement();
         if (failureMode == FailureMode.FAIL_ALTERNATING_INVOCATIONS) {
             if (n % 2 == 0) {
