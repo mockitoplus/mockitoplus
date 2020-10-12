1c8fbabd940d2db07405347b6a4b9a7812a8fa85
Sean C. Sullivan <github@seansullivan.com>
2020-10-11 18:55:09 -0700

    cleanup MockitoPlus.java

--- src/main/java/io/github/mockitoplus/MockitoPlus.java
+++ src/main/java/io/github/mockitoplus/MockitoPlus.java
@@ -1,13 +1,11 @@
 package io.github.mockitoplus;
 
 import org.mockito.Mockito;
-import org.mockito.stubbing.OngoingStubbing;
 
 public class MockitoPlus {
     private MockitoPlus() { /* private constructor */ }
 
     public static <T> MockitoPlusStubbing<T> when(T methodCall) {
-        OngoingStubbing<T> stubbing = Mockito.when(methodCall);
-        return new MockitoPlusStubbing<T>(stubbing);
+        return new MockitoPlusStubbing<T>(Mockito.when(methodCall));
     }
 }
