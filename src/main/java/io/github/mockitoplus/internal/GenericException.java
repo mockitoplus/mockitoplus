6ce8d873fb32d5cefc5af101fd1f1ede59dbe400
Sean C. Sullivan <github@seansullivan.com>
2020-10-11 21:35:21 -0700

    add javadoc comment

--- src/main/java/io/github/mockitoplus/internal/GenericException.java
+++ src/main/java/io/github/mockitoplus/internal/GenericException.java
@@ -1,5 +1,6 @@
 package io.github.mockitoplus.internal;
 
+/* internal exception class */
 public final class GenericException extends Exception {
     public GenericException() {
         super("generic exception");
